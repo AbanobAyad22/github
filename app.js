649f4bc02230478b8971be9998261c2957bdbe28
abanoub22 <abanoub.ayad.samy@gmail.com>
2026-05-02 02:44:14 +0300

    ddd

--- app.js
+++ app.js
@@ -1,12 +1,53 @@
-// app.js
-const http = require('http');
-const PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const request = require('request');
+const wikip = require('wiki-infobox-parser');
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, Kubernetes with Kustomize!\n');
+//ejs
+app.set("view engine", 'ejs');
+
+//routes
+app.get('/', (req,res) =>{
+    res.render('index');
 });
 
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.get('/index', (req,response) =>{
+    let url = "https://en.wikipedia.org/w/api.php"
+    let params = {
+        action: "opensearch",
+        search: req.query.person,
+        limit: "1",
+        namespace: "0",
+        format: "json"
+    }
+
+    url = url + "?"
+    Object.keys(params).forEach( (key) => {
+        url += '&' + key + '=' + params[key]; 
+    });
+
+    //get wikip search string
+    request(url,(err,res, body) =>{
+        if(err) {
+            response.redirect('404');
+        }
+            result = JSON.parse(body);
+            x = result[3][0];
+            x = x.substring(30, x.length); 
+            //get wikip json
+            wikip(x , (err, final) => {
+                if (err){
+                    response.redirect('404');
+                }
+                else{
+                    const answers = final;
+                    response.send(answers);
+                }
+            });
+    });
+
+    
 });
+
+//port
+app.listen(3000, console.log("Listening at port 3000..."))
