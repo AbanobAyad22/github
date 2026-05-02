ec58340a9da362cedf2ef93212c593fbd070d9b6
abanoub22 <abanoub.ayad.samy@gmail.com>
2026-05-02 04:04:46 +0300

    ddd

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, Kubernetes with Abanoub! welcome\n');
+    res.end('Hello, Kubernetes with Abanoub!e\n');
 });
 
 server.listen(PORT, '0.0.0.0', () => {
