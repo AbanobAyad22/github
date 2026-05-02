adf8c39a677993f33dec3337ab8378cc7a0f1184
abanoub22 <abanoub.ayad.samy@gmail.com>
2026-05-02 16:23:38 +0300

    ddd

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Express!');
+  res.send('welcome you are now have a pipeline');
 });
 
 app.listen(port, () => {
