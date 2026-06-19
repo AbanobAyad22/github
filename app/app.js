f95d5c982e2a03e89f42d1f8c39e2b23fa6f6702
abanoub22 <abanoub.ayad.samy@gmail.com>
2026-06-19 15:04:26 +0300

    initial my app

---
+++ app/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Define a basic route that sends text to the browser
+app.get('/', (req, res) => {
+    res.send('Hello, Abanoub.');
+});
+
+// Start the server and listen on port 3000
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
