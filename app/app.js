7044fce00f4c615f5fc4541b1235c9d3f035a93e
abanoub22 <abanoub.ayad.samy@gmail.com>
2026-06-19 17:23:06 +0300

    add

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 // Define a basic route that sends text to the browser
 app.get('/', (req, res) => {
-    res.send('Hello, Abanoub.');
+    res.send('Hello, we are finished cicd cource');
 });
 
 // Start the server and listen on port 3000
